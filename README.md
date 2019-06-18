391c16091fa4fc3345cdb11eb86fb99d51f854a6
Kangkang Han <kangkang.han@cootek.cn>
2019-06-18 17:28:38 +0800

    add xzap

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pkg
 go语言工具包
+
+## xzap
+基于zap日志包改造的日志工具
+- 支持按小时自动切割日志文件
+- 自动把error级别日志单独输出到 outputPath.log.error 文件
+
+```$xslt
+out := []string{"stderr","/ssd/outputPath.log"}
+zlog,err := xzap.NewZLog(out,zapcore.DebugLevel)
+if err != nil {
+    panic(err)
+}
+
+for {
+    zlog.Debug("hello debug")
+    zlog.Info("hello info")
+    zlog.Error("hello error")
+    time.Sleep(10 * time.Second)
+}
+
+//输出
+/ssd/outputPath.log
+/ssd/outputPath.log.error
+```
---
+++ xzap/xzap.go
@@ -0,0 +1,155 @@
+package xzap
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+	"sync"
+	"time"
+)
+
+type ZLog struct {
+	log *zap.Logger
+	outputPaths []string
+	closeOutputPathsFunc func()
+	errorOutPaths []string
+	closeErrorOutPathsFunc func()
+	level zapcore.Level
+}
+
+var once = &sync.Once{}
+var zl	*ZLog
+var err error
+
+
+func NewZLog(outputPaths []string, level zapcore.Level) (*ZLog,error){
+	if zl != nil {
+		return zl,err
+	}
+
+	errorOutPaths := []string{}
+	for _,v := range outputPaths {
+		if v == "stderr" || v == "stdin" || v == "stdout" {
+			continue
+		}
+		errorOutPaths = append(errorOutPaths, v + ".error")
+	}
+
+	zl = &ZLog{
+		outputPaths:outputPaths,
+		errorOutPaths:errorOutPaths,
+		level:level,
+	}
+	err = zl.init()
+	if err != nil {
+		return zl,err
+	}
+	fmt.Println("new zlog",outputPaths, errorOutPaths)
+	once.Do(zl.splitByTime)
+	return zl,err
+}
+
+func (this *ZLog) init() (err error) {
+	var sink zapcore.WriteSyncer
+	//初始化日志输入文件
+	sink, this.closeOutputPathsFunc, err = zap.Open(this.outputPaths...)
+	if err != nil {
+		this.closeOutputPathsFunc()
+		return err
+	}
+	allWriter := zapcore.AddSync(sink)
+	//初始化错误日志输入文件
+	sink, this.closeErrorOutPathsFunc, err = zap.Open(this.errorOutPaths...)
+	if err != nil {
+		this.closeErrorOutPathsFunc()
+		return err
+	}
+	errorWriter := zapcore.AddSync(sink)
+	//初始化日志格式配置
+	config := zapcore.EncoderConfig{
+		TimeKey:        "ts",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime: this.timeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+	encoder := zapcore.NewJSONEncoder(config)
+
+	//一次写行为到多个输出端
+	core := zapcore.NewTee(
+		zapcore.NewCore(encoder, allWriter, zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl >= this.level
+		})),
+		zapcore.NewCore(encoder, errorWriter, zap.LevelEnablerFunc(func(lvl zapcore.Level) bool {
+			return lvl >= zapcore.ErrorLevel
+		})),
+	)
+
+	this.log = zap.New(core)
+	this.log = this.log.WithOptions(zap.AddCallerSkip(1))
+
+	return
+}
+
+func (this *ZLog) timeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05.000000"))
+}
+
+func (this *ZLog) Debug(msg string, fields ...zap.Field) {
+	this.log.Debug(msg, fields ...)
+}
+
+func (this *ZLog) Info(msg string, fields ...zap.Field) {
+	this.log.Info(msg, fields ...)
+}
+
+func (this *ZLog) Error(msg string, fields ...zap.Field) {
+	this.log.Error(msg, fields ...)
+}
+
+func (this *ZLog) splitByTime() {
+	fmt.Println("do splitByTime")
+	go func() {
+		var lastSplitHour = -1
+		for {
+			time.Sleep(200 * time.Millisecond)
+
+			//每分钟写入一个测试日志
+			if time.Now().Second() == 0 {
+				this.Debug("zlog")
+			}
+
+			//整点切换文件
+			if time.Now().Minute() == 59 {
+				currHour := time.Now().Hour()
+				if currHour == lastSplitHour {
+					continue
+				}
+				lastSplitHour = currHour
+				for _,file := range this.outputPaths{
+					_,err := os.Stat(file)
+					if err == nil {
+						newFile := file + "." + time.Now().Format("2006-01-02_15")
+						err = os.Rename(file, newFile)
+						if err != nil {
+							fmt.Println(err)
+						}else{
+							fmt.Println("RenameFile", newFile)
+						}
+					}
+				}
+				this.log.Sync()
+				this.closeOutputPathsFunc()
+				this.closeErrorOutPathsFunc()
+				this.init()
+			}
+		}
+	}()
+}
